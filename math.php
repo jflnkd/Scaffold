26d2e1545bcba1b7baac5bc16c9b272d6874ac85
jflnkd <99005533+jflnkd@users.noreply.github.com>
2022-02-24 16:08:29 +0800

    Update math.php

--- math.php
+++ math.php
@@ -2,12 +2,17 @@
 <head> <title> Math </title>
 </head>
 
-<body background="271749033_1738777693149655_8901752631479795054_n.jpg" style="background-size:100%">
+<body background="271749033_1738777693149655_8901752631479795054_n.jpg"> 
 <center>
 <br><br>
 <br> <br> <br> 
-<br><font face="Georgia" color="white" size="30"><b><u> "MATH" </u></b></font>
-<br>
+<br><p><font face="Georgia" color="white" size="30"><b><u> "Third Quarter Performance Task- Message of HOPE" </u></b></font></p>
+
+<p><center><font face="Georgia" color="white" size="27"><u><b>"ARTWORK"</b></u></font><br><img src="274591338_670028337640845_8100221865402871686_n.gif"></p>
+
+<p><center><font face="Georgia" color="white" size="27"><u><b>"VIDEO"</b></u></font><video width="320" height="240" controls>
+  <source src="video-1638877645.mp4" type="video/mp4">
+</video>
 
 
 
