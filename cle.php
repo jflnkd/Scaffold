7bcacd19fb9a0ac349981ee3dacb68d456c84e08
jflnkd <99005533+jflnkd@users.noreply.github.com>
2022-02-25 18:13:37 +0800

    Update cle.php

--- cle.php
+++ cle.php
@@ -1,13 +1,15 @@
-<html>
+html>
 <head> <title> Cle </title>
 </head>
 
-<body background="271749033_1738777693149655_8901752631479795054_n.jpg" style="background-size:100%">
+<body background="271749033_1738777693149655_8901752631479795054_n.jpg" style="background-size:100%;"> 
 <center>
 <br><br>
 <br> <br> <br> 
-<br><font face="Georgia" color="white" size="30"><b><u> "CLE" </u></b></font>
-<br>
+<br><p><font face="Georgia" color="white" size="30"><b><u>"Third Quarter Performance Task"</u></b></font></p>
+<br><br>
+<p><center><font face="Georgia" color="white" size="27"><b><u>" ADVOCACY PROJECT"</u</b></font<p>
+<br><br><a href="CLE-3RD-QTR-PT-9F.pdf">click here</a>
 
 
 
