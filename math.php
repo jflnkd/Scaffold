fe38844acf0b4c0c1be11045ccb63b1ff4ad8fc3
jflnkd <99005533+jflnkd@users.noreply.github.com>
2022-02-25 22:05:58 +0800

    Update math.php

--- math.php
+++ math.php
@@ -11,7 +11,7 @@
 <p><br><center><font face="Georgia" color="white" size="27"><u><b>"ARTWORK"</b></u></font><br><br><img src=""></p>
 
 <p><br><center><font face="Georgia" color="white" size="27"><u><b>"VIDEO"</b></u></font><br><br><video width="320" height="240" controls>
-  <source src="videofilename.mp4" type="video/mp4">
+  <source src="video-1645797285.mp4" type="video/mp4">
 </video>
 <br><a href="https://youtu.be/vFpyWBJ1h4o">link</a>
 
