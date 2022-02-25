d31d70540405a47c4a407131e0fab7ad21636b9e
jflnkd <99005533+jflnkd@users.noreply.github.com>
2022-02-25 18:06:51 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -2,12 +2,14 @@
 <head> <title> Cae </title>
 </head>
 
-<body background="271749033_1738777693149655_8901752631479795054_n.jpg" style="background-size:100%">
+<body background="271749033_1738777693149655_8901752631479795054_n.jpg" style="background-size:100%;"> 
 <center>
 <br><br>
 <br> <br> <br> 
-<br><font face="Georgia" color="white" size="30"><b><u> "CAE" </u></b></font>
-<br>
+<br><p><font face="Georgia" color="white" size="30"><b><u> "3RD QTR: MAJOR PT CRITIQUE PAPER" </u></b></font></p>
+<br><br>
+<p><center><font face="Georgia" color="white" size="27"><b><u>"CRITIQUE PAPER"</u></b></font</p>
+<br><br><img src="273734512_961835554466816_7747085428715294502_n.png">
 
 
 
